107508f9437a4f22dbed3e6937a97ea2bd98db60
rcam3937 <rcamferdam@harding.edu>
2023-02-11 16:34:14 -0600

    compine comands

--- src/main/java/frc/robot/commands/RunClimber.java
+++ src/main/java/frc/robot/commands/RunClimber.java
@@ -6,17 +6,20 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Climber;
 
 public class RunClimber extends CommandBase {
   private final Climber s_Climber;
   private final XboxController xboxController;
+  private final Arm s_Arm;
   /** Creates a new RunClimber. */
-  public RunClimber(Climber s_Climber, XboxController xboxController){
+  public RunClimber(Climber s_Climber, XboxController xboxController, Arm s_Arm){
     this.xboxController = xboxController;
     this.s_Climber = s_Climber;
+    this.s_Arm = s_Arm;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(s_Climber);
+    addRequirements(s_Climber, s_Arm);
   }
 
   // Called when the command is initially scheduled.
@@ -26,15 +29,17 @@ public class RunClimber extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    /*if(xboxController.getRawButton(1)){
+    if(xboxController.getRawButton(8)){
+      s_Arm.setShoulder(0);
+      s_Arm.setExtension(50);
       s_Climber.setClimber(4);
     }
-    else if(xboxController.getRawButton(2)){
+    else if(xboxController.getRawButton(7)){
       s_Climber.setClimber(15);
-    }*/
-    if(xboxController.getRawButton(3)){
-      s_Climber.runClimber(1);
     }
+    /*if(xboxController.getRawButton(3)){
+      s_Climber.runClimber(1);
+    }*/
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/SetIntake.java
+++ src/main/java/frc/robot/commands/SetIntake.java
@@ -6,17 +6,20 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Intake;
 
-public class RunWrist extends CommandBase {
+public class SetIntake extends CommandBase {
   private final Intake s_Intake;
   private final XboxController xboxController;
+  private final Arm s_Arm;
   /** Creates a new RunWrist. */
-  public RunWrist(Intake s_Intake, XboxController xboxController) {
+  public SetIntake(Intake s_Intake, XboxController xboxController , Arm s_Arm) {
     this.xboxController = xboxController;
     this.s_Intake = s_Intake;
+    this.s_Arm = s_Arm;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(s_Intake);
+    addRequirements(s_Intake, s_Arm);
   }
 
   // Called when the command is initially scheduled.
@@ -26,14 +29,19 @@ public class RunWrist extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(xboxController.getRawButton(8)){
+    if(xboxController.getRawButton(1)){
+      s_Arm.setShoulder(0);
+      s_Arm.setExtension(0);
       s_Intake.setWrist(5);
-      System.out.println("wrist");
     }
-    else if(xboxController.getRawButton(7)){
+    else if(xboxController.getRawButton(4)){
+      s_Arm.setShoulder(0);
+      s_Arm.setExtension(0);
       s_Intake.setWrist(15);
     }
     else if(xboxController.getRawButton(11)){
+      s_Arm.setShoulder(0);
+      s_Arm.setExtension(50);
       s_Intake.setWrist(15);
     }
   }
