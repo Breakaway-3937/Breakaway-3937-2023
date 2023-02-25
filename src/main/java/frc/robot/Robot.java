87d97a6e4a2d6e80ea2ab05e7399cbb632a7ee84
Jack Pace <jpace2@harding.edu>
2023-02-24 22:14:42 -0600

    testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,20 +4,9 @@
 
 package frc.robot;
 
-import java.util.List;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -37,10 +26,6 @@ public class Robot extends TimedRobot {
 
   private PowerDistribution powerDistribution;
 
-  private Field2d m_field = new Field2d();
-
-  private Trajectory m_trajectory;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -54,17 +39,6 @@ public class Robot extends TimedRobot {
     powerDistribution = new PowerDistribution(25, ModuleType.kRev);
     powerDistribution.setSwitchableChannel(true);
     powerDistribution.clearStickyFaults();
-    m_trajectory =
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            new Pose2d(3, 0, Rotation2d.fromDegrees(0)),
-            new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0)));
-
-    // Create and push Field2d to SmartDashboard.
-    m_field = new Field2d();
-    // Push the trajectory to Field2d.
-    m_field.getObject("traj").setTrajectory(m_trajectory);
   }
 
   /**
@@ -81,7 +55,6 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    SmartDashboard.putData(m_field);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
