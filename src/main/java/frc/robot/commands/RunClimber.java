cc1d9e9a558348467dd16b044c8f9d52c874b127
Jack Pace <jpace2@harding.edu>
2023-03-20 22:22:39 -0500

    removing unused code

--- src/main/java/frc/robot/commands/RunClimber.java
+++ src/main/java/frc/robot/commands/RunClimber.java
@@ -11,8 +11,6 @@ import frc.robot.subsystems.Climber;
 public class RunClimber extends CommandBase {
   private final Climber s_Climber;
   private final XboxController xboxController;
-  private boolean flag;
-  public static boolean dropArm;
  
   /** Creates a new RunClimber. */
   public RunClimber(Climber s_Climber, XboxController xboxController){
@@ -24,10 +22,7 @@ public class RunClimber extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    flag = false;
-    dropArm = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -38,11 +33,7 @@ public class RunClimber extends CommandBase {
     else if(xboxController.getRawAxis(2) > 0.5){
       s_Climber.setClimberSpeed();
       s_Climber.setClimber(-5); 
-      flag = true;
     }
-    /*if(s_Climber.getClimber() < 30 && flag){
-      dropArm = true;
-    }*/
   }
 
   // Called once the command ends or is interrupted.
