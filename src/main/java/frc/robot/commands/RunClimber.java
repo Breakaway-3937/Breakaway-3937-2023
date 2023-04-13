909af0346b92afb998598aef24dde918501e5af0
Jack Pace <jpace2@harding.edu>
2023-04-13 16:43:13 -0500

    setting climber to 0 for match

--- src/main/java/frc/robot/commands/RunClimber.java
+++ src/main/java/frc/robot/commands/RunClimber.java
@@ -22,7 +22,9 @@ public class RunClimber extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    s_Climber.setClimber(0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
