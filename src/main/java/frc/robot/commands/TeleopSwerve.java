f6b63d0373ce1e2ffcfe02310b580cb3978106d2
Jack Pace <jpace2@harding.edu>
2023-07-06 19:22:34 -0500

    foc toggle

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -17,7 +18,7 @@ public class TeleopSwerve extends CommandBase {
     private Drivetrain s_Drivetrain;
     private Joystick translationalController;
     private Joystick rotationalController;
-    private Joystick buttonGrid;
+    private XboxController xboxController;
     private int translationAxis;
     private int strafeAxis;
     private int rotationAxis;
@@ -27,13 +28,13 @@ public class TeleopSwerve extends CommandBase {
     /**
      * Driver control
      */
-    public TeleopSwerve(Drivetrain s_Drivetrain, Joystick controller, Joystick controller1, Joystick buttonGrid, int translationAxis, int strafeAxis, int rotationAxis, boolean fieldRelative, boolean openLoop) {
+    public TeleopSwerve(Drivetrain s_Drivetrain, Joystick controller, Joystick controller1, XboxController xboxController, int translationAxis, int strafeAxis, int rotationAxis, boolean fieldRelative, boolean openLoop) {
         this.s_Drivetrain = s_Drivetrain;
         addRequirements(s_Drivetrain);
 
         this.translationalController = controller;
         this.rotationalController = controller1;
-        this.buttonGrid = buttonGrid;
+        this.xboxController = xboxController;
         this.translationAxis = translationAxis;
         this.strafeAxis = strafeAxis;
         this.rotationAxis = rotationAxis;
@@ -54,7 +55,7 @@ public class TeleopSwerve extends CommandBase {
 
         translation = new Translation2d(yAxis, xAxis).times(Constants.Drivetrain.MAX_SPEED);
         rotation = rAxis * Constants.Drivetrain.MAX_ANGULAR_VELOCITY;
-        foc = buttonGrid.getRawButton(1);
+        foc = xboxController.getRawButton(9);
         s_Drivetrain.drive(translation, rotation, fieldRelative, openLoop, foc);
     }
 }
